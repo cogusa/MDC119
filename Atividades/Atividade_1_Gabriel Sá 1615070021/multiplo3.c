b20e28b31bd9ec8c3459ac38d2232b708f4ff38d
Gabriel Sá 1614070021 <61788959+cogusa@users.noreply.github.com>
2020-04-22 20:19:31 -0300

    Add files via upload

---
+++ Atividades/Atividade_1_Gabriel Sá 1615070021/multiplo3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int n;
+	for(n=0;n<101;n=n+3){
+		printf("\n%i",n);
+	}
+	
+	getchar();
+	printf("\n\n");
+	
+	
+	return 0;
+}
